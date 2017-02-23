9afcaa762b685398481058ef685e854bd3b75a46
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-02-23 17:13:10 +0100

    removed comment from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,30 +4,30 @@ require "faker"
 
 #populate ingredients === done
 
-# info = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
-# info["drinks"].each do |ingredient|
-#   name_ingredient = ingredient["strIngredient1"]
-#   Ingredient.create(name: name_ingredient)
-# end
+info = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+info["drinks"].each do |ingredient|
+  name_ingredient = ingredient["strIngredient1"]
+  Ingredient.create(name: name_ingredient)
+end
 
 # get a random number of ingredients and assing each with a dose to a cocktail 50 times === done
 
-# 50.times do |i|
-#   cocktail_name = Faker::Name.first_name
-#   cocktail = Cocktail.new(name: cocktail_name)
-#   cocktail.save
-#   rand_number = rand(3) + 2
-#   rand_number.times do |i|
-#     offset = rand(Ingredient.count)
-#     rand_record = Ingredient.offset(offset).first
+100.times do |i|
+  cocktail_name = Faker::Name.first_name
+  cocktail = Cocktail.new(name: cocktail_name)
+  cocktail.save
+  rand_number = rand(3) + 2
+  rand_number.times do |i|
+    offset = rand(Ingredient.count)
+    rand_record = Ingredient.offset(offset).first
 
-#     dose_description = Faker::Lorem.paragraph
-#     dose = Dose.new(description: dose_description)
-#     dose.ingredient = rand_record
-#     dose.cocktail = cocktail
-#     dose.save
-#   end
-# end
+    dose_description = Faker::Lorem.paragraph
+    dose = Dose.new(description: dose_description)
+    dose.ingredient = rand_record
+    dose.cocktail = cocktail
+    dose.save
+  end
+end
 
 
 
